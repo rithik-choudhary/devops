32d5f079a243b112bfcd84cb2ea8c5e56c7780a0
rithik-choudhary <159942363+rithik-choudhary@users.noreply.github.com>
2025-07-31 13:19:10 +0530

    Update provider.tf

--- AWS_Service/provider.tf
+++ AWS_Service/provider.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region     = "eu-north-1"
-  access_key = "AKIAVH33STW6WCVCIJ4D"
-  secret_key = "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"
+  region     = "xxx"
+  access_key = "xxxx"
+  secret_key = "xxx"
 }
 
 # provider "aws" {
-#   region     = "ap-southeast-1"
-#   access_key = "AKIATNSP23W4PWYWWXWX"
-#   secret_key = "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"
+#   region     = "ap-xx-1"
+#   access_key = "xxxx"
+#   secret_key = "xxx/xx+xx"
 # }
 
 
