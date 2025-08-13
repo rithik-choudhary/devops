3a76fa81c6000f3f296ac358a09517f7c0d9a786
sakshi7397 <sakshi@transfi.com>
2025-08-13 12:09:38 +0530

    Update variable.tf

--- AWS_Service/variable.tf
+++ AWS_Service/variable.tf
@@ -24,9 +24,9 @@ variable "qa_security_group" {
 variable "subnet_name" {
     type = list(string)
 }
-# variable "qa_application_lb" {
-#   type = string
-# }
+variable "qa_application_lb" {
+  type = string
+}
 variable "ECS_service_name" {
   type = string
 }
@@ -36,20 +36,20 @@ variable "container_name" {
 variable "container_port" {
     type = number
 }
-# variable "target_group" {
-#     type = string  
-# }
-# variable "domain_name" {
-#   type = string
-#   description = "Domain name for the service (e.g., api.example.com)"
-# }
-# variable "rule_name" {
-#   type = string
-#   description = "Name for the listener rule (e.g., new-api-rule)"
-# }
-# variable "listener_rule_priority" {
-#   type        = number
-#   description = "Priority for the listener rule (1-50000)"
-# }
+variable "target_group" {
+    type = string  
+ }
+variable "domain_name" {
+  type = string
+  description = "Domain name for the service (e.g., api.example.com)"
+}
+variable "rule_name" {
+  type = string
+  description = "Name for the listener rule (e.g., new-api-rule)"
+}
+variable "listener_rule_priority" {
+  type        = number
+  description = "Priority for the listener rule (1-50000)"
+}
 
 
