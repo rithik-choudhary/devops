4bab904b12b200b30e7bfe680ce657ebb5625ffb
sakshi7397 <sakshi@transfi.com>
2025-08-13 12:27:09 +0530

    Update variable.tf

--- Worker-Cluster-Service/variable.tf
+++ Worker-Cluster-Service/variable.tf
@@ -1,1 +1,35 @@
-hi
+variable "ECR_repository_name" {
+  type = string
+}
+
+variable "ECS_task_defiantion_name" {
+  type = string
+}
+
+variable "qa_cluster" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "qa_security_group" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = list(string)
+}
+
+variable "ECS_service_name" {
+  type = string
+}
+
+variable "container_name" {
+  type = string
+}
+
+variable "container_port" {
+  type = number
+}
